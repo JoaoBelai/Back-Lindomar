3d5454d22fa84e4ca670aaebb20378324988930d
João Belai <joaopbelai@gmail.com>
2025-08-07 16:40:32 -0300

    primeiro post

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Autor (models.Model):
+    nome = models.CharField(max_length=255, null=False, blank=False)
+    sobrenome = models.CharField(max_length=255, null=False, blank=False)
+    data_nasc = models.DateField(null=False, blank=False)
+    nacionalidade = models.CharField(max_length=50, null=False, blank=False)
+    biografia = models.TextField(null=False, blank=False)
 
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Autor
+
+class AutorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = '__all__'
+    
+    
