cb5bc610d97b5ffa9752d4a6edbe2e84838b9f4c
João Belai <joaopbelai@gmail.com>
2025-08-21 14:51:39 -0300

    get e post livros e editoras

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('autores', AutoresView.as_view()),
-    path('autoresMetodo', listar_autores),
+    # path('autores', AutoresView.as_view()),
+    #path('livros', LivrosView.as_view()),
+    #path('editoras', EditorasView.as_view()),
+    path('autores', listar_criar_autores),
+    path('editoras', listar_criar_editoras),
+    path('livros', listar_criar_livros),
     path('autores/<int:pk>', AutoresRetriveUpdateDestroy.as_view())
 ]
 
--- api/views.py
+++ api/views.py
@@ -3,16 +3,24 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from .models import Autor
-from .serializers import AutorSerializers
+from .models import Autor, Editora, Livro
+from .serializers import AutorSerializers, EditoraSerializers, LivroSerializers
 
-class AutoresView(ListCreateAPIView):
-    queryset = Autor.objects.all()
-    serializer_class = AutorSerializers
+# class AutoresView(ListCreateAPIView):
+#     queryset = Autor.objects.all()
+#     serializer_class = AutorSerializers
+
+# class EditorasView(ListCreateAPIView):
+#     queryset = Editora.objects.all()
+#     serializer_class = EditoraSerializers
+
+# class LivrosView(ListCreateAPIView):
+#     queryset = Livro.objects.all()
+#     serializer_class = LivroSerializers
 
 #Faz a mesma coisa do de cima, mas em método é mais manual, não genérico
 @api_view(['GET', 'POST'])
-def listar_autores(request):
+def listar_criar_autores(request):
     if request.method == 'GET':
         queryset = Autor.objects.all()
         serializer = AutorSerializers(queryset, many = True)
@@ -25,7 +33,36 @@ def listar_autores(request):
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET', 'POST'])
+def listar_criar_editoras(request):
+    if request.method == 'GET':
+        queryset = Editora.objects.all()
+        serializer = EditoraSerializers(queryset, many = True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = EditoraSerializers(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'POST'])
+def listar_criar_livros(request):
+    if request.method == 'GET':
+        queryset = Livro.objects.all()
+        serializer = LivroSerializers(queryset, many = True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = LivroSerializers(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 class AutoresRetriveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Autor.objects.all()
