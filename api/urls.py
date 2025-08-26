5f44cf216e179eb762caea6abcd2fc23d59fc051
João Belai <joaopbelai@gmail.com>
2025-08-26 15:00:38 -0300

    crud completo

--- api/urls.py
+++ api/urls.py
@@ -9,11 +9,17 @@ urlpatterns = [
     path('autores', get_autores),
     path('autores/<int:pk>', get_autor),
     path('autores/post', post_autores),
+    path('autores/put/<int:pk>', put_autor),
+    path('autores/delete/<int:pk>', delete_autor),
     path('editoras', get_editoras),
     path('editoras/<int:pk>', get_editora),
     path('editoras/post', post_editoras),
+    path('editoras/put/<int:pk>', put_editora),
+    path('editoras/delete/<int:pk>', delete_editora),
     path('livros', get_livros),
     path('livros/<int:pk>', get_livro),
     path('livros/post', post_livros),
+    path('livros/put/<int:pk>', put_livro),
+    path('livros/delete/<int:pk>', delete_livro),
 ]
 
--- api/views.py
+++ api/views.py
@@ -18,6 +18,10 @@ from .serializers import AutorSerializers, EditoraSerializers, LivroSerializers
 #     queryset = Livro.objects.all()
 #     serializer_class = LivroSerializers
 
+# class AutoresRetriveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+#     queryset = Autor.objects.all()
+#     serializer_class = AutorSerializers
+
 #Faz a mesma coisa do de cima, mas em método é mais manual, não genérico
 @api_view(['GET'])
 def get_autores(request):
@@ -101,7 +105,112 @@ def post_livros(request):
         else:
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT'])
+def put_autor(request, pk):
+    if request.method == 'GET':
+        queryset = Autor.objects.get(pk=pk)
+        serializer = AutorSerializers(queryset)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        try:
+            autor = Autor.objects.get(pk=pk)
+        except Autor.DoesNotExist:
+            return Response({'error' : 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = AutorSerializers(autor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class AutoresRetriveUpdateDestroy(RetrieveUpdateDestroyAPIView):
-    queryset = Autor.objects.all()
-    serializer_class = AutorSerializers
+
+@api_view(['GET', 'PUT'])
+def put_livro(request, pk):
+    if request.method == 'GET':
+        queryset = Livro.objects.get(pk=pk)
+        serializer = LivroSerializers(queryset)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        try:
+            livro  = Livro.objects.get(pk=pk)
+        except Livro.DoesNotExist:
+            return Response({'erro' : 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = LivroSerializers(livro, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET', 'PUT'])
+def put_editora(request, pk):
+    if request.method == 'GET':
+        queryset = Editora.objects.get(pk=pk)
+        serirializer = EditoraSerializers(queryset)
+        return Response(serirializer.data)
+    
+    elif request.method == 'PUT':
+        try:
+            editora = Editora.objects.get(pk=pk) 
+        except Editora.DoesNotExist:
+            return Response({'erro': 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serirializer = EditoraSerializers(editora, data=request.data)
+        if serirializer.is_valid():
+            serirializer.save()
+            return Response(serirializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serirializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'DELETE'])
+def delete_autor(request, pk):
+    if request.method == 'GET':
+        queryset = Autor.objects.get(pk=pk)
+        serializer = AutorSerializers(queryset)
+        return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        try:
+            autor = Autor.objects.get(pk=pk) 
+        except Autor.DoesNotExist:
+            return Response({'erro': 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        autor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+@api_view(['GET', 'DELETE'])
+def delete_livro(request, pk):
+    if request.method == 'GET':
+        queryset = Livro.objects.get(pk=pk)
+        serializer = LivroSerializers(queryset)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        try: 
+            livro = Livro.objects.get(pk=pk)
+        except Livro.DoesNotExist:
+            return Response({'erro', 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        livro.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'DELETE'])
+def delete_editora(request, pk):
+    if request.method == 'GET':
+        queryset = Editora.objects.get(pk=pk)
+        serializer = EditoraSerializers(queryset)
+        return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        try: 
+            editora = Editora.objects.get(pk=pk)
+        except Editora.DoesNotExist:
+            return Response({'erro': 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        editora.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
